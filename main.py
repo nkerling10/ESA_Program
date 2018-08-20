421949b39cad38fdbabd45ee78bc6614963b2e58
Nick <nsk10@live.com>
2018-08-20 18:19:13 -0500

    Major Overhaul

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import wmi
 import getpass
 import paramiko
 import requests
+import sqlite3
+import uuid
+import hashlib
 from bs4 import BeautifulSoup
 from datetime import datetime
 from twilio.rest import Client
@@ -23,56 +26,66 @@ def get_hostname_ip():  # Function that obtains the host name and ip address of 
     return host_name, host_ip
 
 
-def menu():     # Function that displays the standard menu for non-admin users
+def uname_register():    # Function to register a new user
+    register_uname = ""
+    conn = sqlite3.connect('masterDB.db')
+    c = conn.cursor()
+    os.system('cls')
+    print("User creation\n-----------------------------------")
+    while register_uname == "":
+        name_check = ""
+        register_uname = input("Enter desired Username: ")
+        c.execute('select * from credentials where username =?', (register_uname,))
+        row = c.fetchall()
+        for col in row:
+            name_check = col[0]
+        if register_uname == "":
+            print("Username cannot be null.\n")
+            register_uname = ""
+        elif name_check == "":
+            return register_uname
+        elif name_check == register_uname:
+            print("Sorry, that username is already taken.\n")
+            register_uname = ""
+        else:
+            print("Sorry, unknown error.\n")
+            register_uname = ""
+    conn.commit()
+    c.close()
+
+
+def pwd_register():
+    register_pwd = ""
+    while register_pwd == "":
+        register_pwd = getpass.getpass("Create a password: ")
+        if register_pwd == "":
+            print("Password cannot be null.\n")
+    gensalt = uuid.uuid4().hex
+    register_pwd = hashlib.sha256(gensalt.encode() + register_pwd.encode()).hexdigest()
+    return register_pwd, gensalt
+
+
+def register_user():    # Function to register a new user
+    register_uname = uname_register()
+    register_pwd, salt = pwd_register()
+    print("\nValidating credentials..")
+    conn = sqlite3.connect('masterDB.db')
+    c = conn.cursor()
+    c.execute("insert into credentials (username, password, salt, admin) values (?, ?, ?, ?)", (register_uname, register_pwd, salt, "False"))
+    host_name, host_ip = get_hostname_ip()
+    c.execute("insert into login_history (username, hostname, ip, datetime, result) values (?, ?, ?, ?, ?)", (register_uname, host_name, host_ip, str(datetime.now()), "ACCOUNT CREATION"))
+    conn.commit()
+    print("\nUser successfully created.")
+    logged_in = register_uname
+    conn.commit()
+    c.close()
+    return logged_in
+
+
+def log_out():  # Function to log the user out
     logged_in = ""
-    while True:
-        os.system('cls')
-        print("Logged in as: " + logged_in)
-        print("\nMenu\n-----------------------------------")
-        print("1) Login")
-        print("2) Create account")
-        print("3) Log out")
-        print("4) Exit")
-        choice = input("\n>> ")
-        if choice == "1":
-            logged_in = cred_check()
-        elif choice == "2":
-            logged_in = register_user()
-        elif choice == "3":
-            if logged_in != "":
-                logged_in = log_out()
-            else:
-                print("No user is logged in!\n")
-        elif choice == "4":
-            quit()
-        else:
-            print("Please enter a valid choice.")
-
-
-def menu_admin(logged_in):      # Function that displays the admin specific selection menu
-    while True:
-        os.system('cls')
-        print("Logged in as: " + logged_in + " [ADMIN]")
-        print("Menu\n-----------------------------------")
-        print("1) Admin panel")
-        print("2) Create account")
-        print("3) Log out")
-        print("4) Exit\n")
-        choice = input(">> ")
-        if choice == "1":
-            admin_panel(logged_in)
-        elif choice == "2":
-            logged_in = register_user()
-        elif choice == "3":
-            if logged_in != "":
-                logged_in = log_out()
-                menu()
-            else:
-                print("No user is logged in!\n")
-        elif choice == "4":
-            quit()
-        else:
-            print("Please enter a valid choice.")
+    print("\nYou have successfully logged out.\n")
+    return logged_in
 
 
 def admin_panel(logged_in):     # Function that displays the admin only menu
@@ -168,10 +181,78 @@ def admin_panel(logged_in):     # Function that displays the admin only menu
             print("\nPlease enter a valid choice.\n")
 
 
-def cred_check():   # Function to handle user login
-    os.system('cls')
+def menu():     # Function that displays the standard menu for non-admin users
+    logged_in = ""
+    while True:
+        os.system('cls')
+        print("Logged in as: " + logged_in)
+        print("\nMenu\n-----------------------------------")
+        print("1) Login")
+        print("2) Create account")
+        print("3) Log out")
+        print("4) Exit")
+        choice = input("\n>> ")
+        if choice == "1":
+            if logged_in != "":
+                print("\nYou are already logged in!")
+            else:
+                logged_in = cred_check()
+        elif choice == "2":
+            logged_in = register_user()
+        elif choice == "3":
+            if logged_in != "":
+                logged_in = log_out()
+            else:
+                print("No user is logged in!\n")
+        elif choice == "4":
+            quit()
+        else:
+            print("Please enter a valid choice.")
+
+
+def menu_admin(logged_in):      # Function that displays the admin specific selection menu
+    while True:
+        os.system('cls')
+        print("Logged in as: " + logged_in + " [ADMIN]")
+        print("Menu\n-----------------------------------")
+        print("1) Admin panel")
+        print("2) Create account")
+        print("3) Log out")
+        print("4) Exit\n")
+        choice = input(">> ")
+        if choice == "1":
+            admin_panel(logged_in)
+        elif choice == "2":
+            logged_in = register_user()
+        elif choice == "3":
+            if logged_in != "":
+                logged_in = log_out()
+                menu()
+            else:
+                print("No user is logged in!\n")
+        elif choice == "4":
+            quit()
+        else:
+            print("Please enter a valid choice.")
+
+
+def two_factor(size=8, chars=string.ascii_uppercase + string.digits):   # Function to generate and send the 2 factor authentication SMS code
+    code = ''.join(random.choice(chars) for _ in range(size))
+    #text_alert_from = +15074811128
+    #text_alert_to = +15074408674
+    #text_body = "Your code is: " + code
+    #client = Client("AC0969b7fcb8bf56748d697f16ef8ee16a", "aa760f3fa534876dd1dd0de36239dc08")
+    #client.messages.create(to=text_alert_to, from_=text_alert_from, body=text_body)
+    print(code)
+    return code
+
+
+def cred_check():  # Function to handle user login
+    lock_uname = ""
+    db_name = ""
+    conn = sqlite3.connect('masterDB.db')
+    c = conn.cursor()
     failed_login = 0
-    logins = dict()
     success = False
     while True:
         while success == False:
@@ -181,133 +262,78 @@ def cred_check():   # Function to handle user login
             pwd_login = getpass.getpass()
             print("\nValidating credentials..")
             time.sleep(2)
-            with open('locked_accounts.txt', 'r') as f:
-                locked_accounts = f.read().splitlines()
-            if uname_login in locked_accounts:
+            c.execute("select * from locked_accounts where username =?", (uname_login,))
+            row = c.fetchall()
+            for col in row:
+                lock_uname = col[0]
+            if lock_uname != "":
                 host_name, host_ip = get_hostname_ip()
-                with open('login_history.txt', 'a') as f:
-                    f.write(uname_login + ", " + host_name + ", " + host_ip + ", " + str(datetime.now()) + ", " + "LOCKED ACCOUNT" + "\n")
-                f.close()
                 print("\nSorry, but your account is locked. Please contact an administrator to unlock your account.")
-                time.sleep(3)
+                c.execute("insert into login_history (username, hostname, ip, datetime, result) values (?, ?, ?, ?, ?)",
+                          (uname_login, host_name, host_ip, str(datetime.now()), "LOCKED ACCOUNT"))
+                conn.commit()
+                lock_uname = ""
                 menu()
-            elif logins[uname_login] == pwd_login:
-                success = True
-                code = two_factor()
-                print("\nPlease enter the code sent to your mobile device, or type EXIT to go back.")
-                twof_code = str(input(">> "))
-                if twof_code == "EXIT":
-                    menu()
-                elif twof_code == code:
-                    host_name, host_ip = get_hostname_ip()
-                    with open('login_history.txt', 'a') as f:
-                        f.write(uname_login + ", " + host_name + ", " + host_ip + ", " + str(datetime.now()) + ", " + "SUCCESS" + "\n")
-                    f.close()
-                    logged_in = uname_login
-                    print("\nValidating credentials..\n")
-                    time.sleep(2)
-                    admin = admin_check(logged_in)
-                    if admin == True:
-                        menu_admin(logged_in)
-                    else:
-                        return logged_in
+            else:
+                c.execute("select * from credentials where username =?", (uname_login,))
+                row = c.fetchall()
+                for col in row:
+                    db_name = col[0]
+                if db_name == "":
+                    print("\nThat username does not exist.\n")
                 else:
-                    host_name, host_ip = get_hostname_ip()
-                    with open('login_history.txt', 'a') as f:
-                        f.write(uname_login + ", " + host_name + ", " + host_ip + ", " + str(datetime.now()) + ", " + "FAILED" + "\n")
-                    f.close()
-                    time.sleep(3)
-                    failed_login += 1
-                    print("\nLogin failed. Attempt ", failed_login, " of 3\n")
-                    time.sleep(3)
-                    success = False
-                    if failed_login == 3:
-                        with open('locked_accounts.txt', 'a') as f:
-                            f.write(uname_login + "\n")
-                        f.close()
-            else:
-                host_name, host_ip = get_hostname_ip()
-                with open('login_history.txt', 'a') as f:
-                    f.write(uname_login + ", " + host_name + ", " + host_ip + ", " + str(datetime.now()) + ", " + "FAILED" + "\n")
-                f.close()
-                failed_login += 1
-                print("\nLogin failed.\n")
-                time.sleep(3)
-                success = False
-                if failed_login == 3:
-                    with open('locked_accounts.txt', 'a') as f:
-                        f.write(uname_login + "\n")
-                    f.close()
-
-
-def two_factor(size=8, chars=string.ascii_uppercase + string.digits):   # Function to generate and send the 2 factor authentication SMS code
-    code = ''.join(random.choice(chars) for _ in range(size))
-    text_alert_from = +15074811128
-    text_alert_to = +15074408674
-    text_body = "Your code is: " + code
-    client = Client("AC0969b7fcb8bf56748d697f16ef8ee16a", "aa760f3fa534876dd1dd0de36239dc08")
-    client.messages.create(to=text_alert_to, from_=text_alert_from, body=text_body)
-    return code
-
-
-def log_out():  # Function to log the user out
-    logged_in = ""
-    print("\nYou have successfully logged out.\n")
-    return logged_in
-
-
-def register_user():    # Function to register a new user
-    uname = load_file('credentials.txt')
-    register_uname = uname_register(uname)
-    register_pwd = pwd_register()
-    print("\nValidating credentials..")
-    time.sleep(2)
-    with open('credentials.txt', 'a') as f:
-        f.write("\n" + register_uname + "," + register_pwd)
-    f.close()
-    print("\nUser successfully created.")
-    time.sleep(2)
-    logged_in = register_uname
-    return logged_in
-
-
-def load_file(file_to_open):    # Function to load a file and read the lines
-    uname = []
-    pwd = []
-    x = 0
-    array = [line.rstrip('\n') for line in open(file_to_open)]
-    for pair in array:
-        pair = array[x]
-        n, p = pair.split(",")
-        uname.append(n)
-        pwd.append(p)
-        x += 1
-    return uname
-
-
-def uname_register(uname):      # Function to register a new username
-    os.system('cls')
-    register_uname = ""
-    print("User creation\n-----------------------------------")
-    while register_uname == "":
-        register_uname = input("Enter desired Username: ")
-        if register_uname == "":
-            print("Username cannot be null.\n")
-        else:
-            if any(register_uname in s for s in uname):
-                print("Sorry, that username is already taken.\n")
-                register_uname = ""
-            else:
-                return register_uname
-
-
-def pwd_register():     # Function to obtain the password used for registration
-    register_pwd = ""
-    while register_pwd == "":
-        register_pwd = getpass.getpass("Create a password: ")
-        if register_pwd == "":
-            print("Password cannot be null.\n")
-    return register_pwd
+                    c.execute("select * from credentials where username =?", (uname_login,))
+                    row = c.fetchall()
+                    for col in row:
+                        db_name = col[0]
+                        db_pass = col[1]
+                        db_salt = col[2]
+                    hashed_pass = hashlib.sha256(db_salt.encode() + pwd_login.encode()).hexdigest()
+                    if db_pass == hashed_pass:
+                        success = True
+                        code = two_factor()
+                        print("\nPlease enter the code sent to your mobile device, or type EXIT to go back.")
+                        twof_code = str(input(">> "))
+                        if twof_code == "EXIT":
+                            break
+                            # menu()
+                        elif twof_code == code:
+                            host_name, host_ip = get_hostname_ip()
+                            c.execute("insert into login_history (username, hostname, ip, datetime, result) values (?, ?, ?, ?, ?)", (uname_login, host_name, host_ip, str(datetime.now()), "SUCCESS"))
+                            conn.commit()
+                            logged_in = uname_login
+                            print("\nValidating credentials..\n")
+                            time.sleep(2)
+                            admin = admin_check(logged_in)
+                            if admin == True:
+                                print(logged_in)
+                                menu_admin(logged_in)
+                            else:
+                                return logged_in
+                        else:
+                            host_name, host_ip = get_hostname_ip()
+                            c.execute("insert into login_history (username, hostname, ip, datetime, result) values (?, ?, ?, ?, ?)", (uname_login, host_name, host_ip, str(datetime.now()), "FAILURE"))
+                            conn.commit()
+                            time.sleep(3)
+                            failed_login += 1
+                            print("\nLogin failed. Attempt ", failed_login, " of 3\n")
+                            time.sleep(3)
+                            if failed_login == 3:
+                                c.execute("insert into locked_accounts (username, status) values (?, ?)", (uname_login, "True"))
+                            conn.commit()
+                    else:
+                        host_name, host_ip = get_hostname_ip()
+                        c.execute("insert into login_history (username, hostname, ip, datetime, result) values (?, ?, ?, ?, ?)", (uname_login, host_name, host_ip, str(datetime.now()), "FAILURE"))
+                        conn.commit()
+                        failed_login += 1
+                        print("\nLogin failed. Attempt ", failed_login, " of 3\n")
+                        time.sleep(3)
+                        success = False
+                        if failed_login == 3:
+                            c.execute("insert into locked_accounts (username, status) values (?, ?)", (uname_login, "True"))
+                            conn.commit()
+        conn.commit()
+    c.close()
 
 
 def admin_check(logged_in):     # Function to check if the user logging in is an admin
